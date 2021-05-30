785a1e8f5206c2b73545d00a913d3d54c71010df
Rodrigo Luiz Duarte <rodrigo.luizduarte@gmail.com>
2021-05-30 20:21:43 -0300

    Evoluindo o app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -13,8 +13,15 @@ interface SignInCredentials {
   password: string;
 }
 
+interface User {
+  id: string;
+  avatarUrl: string;
+  name: string;
+  email: string;
+}
+
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -22,7 +29,7 @@ interface AuthContextData {
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -43,6 +50,7 @@ export const AuthProvider: React.FC = ({ children }) => {
 
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
       }
 
       setLoading(false);
@@ -57,14 +65,14 @@ export const AuthProvider: React.FC = ({ children }) => {
       password,
     });
 
-    console.log(response.data);
-
-    const { token, userWithoutPassword: user } = response.data;
+    const { token, user } = response.data;
 
     await AsyncStorage.multiSet([
       [SESSION_STORAGE_TOKEN, token],
       [SESSION_STORAGE_USER, JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,62 @@
-import React from "react";
-import { View, Button } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { useAuth } from "../../hooks/auth";
+import api from "../../services/api";
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+} from "./styles";
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatarUrl: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user, signOut } = useAuth();
+
+  const { navigate } = useNavigation();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    api.get("/providers").then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    // navigate("Profile");
+    signOut();
+  }, [signOut]);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatarUrl }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
+    </Container>
   );
 };
 
